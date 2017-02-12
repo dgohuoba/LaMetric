53fa5b2b149c5da173c85ec0e2decd560b2dbd64
dgohuoba <dgohuoba@gmail.com>
2017-02-11 23:22:53 -0600

    test
    test

--- traffic.js
+++ traffic.js
@@ -1,52 +1,24 @@
-// follow modules loaded via `npm i --save request cheerio`
-const request = require('request');
-const cheerio = require('cheerio');
-const http = require('http');
 
-http.createServer((req, res) => {
-  getDueTimes().then(data => {
-    res.writeHead(200, { 'content-type': 'application/json' });
-    res.end(JSON.stringify(data));
-  }).catch(error => {
-    console.error(error);
-    res.writeHead(500, { 'content-type': 'application/json' });
-    res.end(JSON.stringify({ error }));
-  });
-}).listen(3000);
-
-
-function getDueTimes() {
-  // we're returning a promise so we can later reuse in
-  // a web server
-  return new Promise((resolve, reject) => {
-    // make a GET request to get a page of the next due buses
-    request({
-      url: 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=4194%20Fairmeadow%20Dr,%20Round%20Rock,%20TX%2078665&destinations=717%20E%20Parmer%20Ln,%20Austin,%20TX%2078753&key=AIzaSyBn_KKU3YD0YfACFCb_Trm-ld03iupFFlc'
-    }, (error, res, body) => {
-      // if there was something wrong with the request, print it
-      // out and exit the function
-      if (error) {
-        return reject(error);
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
       }
+    };
+    xhr.send();
+};
 
-
-      // now query the DOM for the times (bespoke to your markup)
-      // and return just the text for each of those nodes, note
-      // that to get a _real_ array back, I have to use `.get()`
-      // at the end.
-      const times = 10;
-
-      // resolve the promise: specifically, return the times
-      const icon = "i996"; // our bus icon
-      const data = 
-	  {"frames": [
-        {
-            "text": "Traffic",
-            "icon": "a7267"
-        }
-    ]
-	}
-      resolve(data);
-    });
-  });
-}
+getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback",
+function(err, data) {
+  if (err != null) {
+    alert("Something went wrong: " + err);
+  } else {
+    alert("Your query count: " + data.query.count);
+  }
+});
